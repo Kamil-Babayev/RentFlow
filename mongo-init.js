110b0ba216db93d4f817cc6c9b549617e0cbe2ef
kamilbabaev <babayevkamil03@icloud.com>
2026-03-15 16:38:51 +0400

    feature(US0.1): added docker-compose.infra.yml file which starts the local environment with all needed infrastructure, added scripts for container initialization, added make file with commands to control infrastructure container

---
+++ mongo-init.js
@@ -0,0 +1,9 @@
+db = db.getSiblingDB('mydb');
+
+db.createUser({
+    user: 'myuser',
+    pwd: 'mypassword',
+    roles: [{ role: 'readWrite', db: 'mydb' }]
+});
+
+db.createCollection('mycollection');
